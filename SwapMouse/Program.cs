601a520a6e7f32f1841603d70a4cbf25d0a2d1ff
asfuyao <asfuyao@gmail.com>
2018-05-21 13:57:55 +0800

    增加更换鼠标指针功能

--- SwapMouse/Program.cs
+++ SwapMouse/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.IO;
 
 namespace SwapMouse
 {
@@ -12,16 +13,58 @@ namespace SwapMouse
         public static extern Int32 SwapMouseButton(Int32 bSwap);
         [DllImport("user32.dll")]
         public static extern Int32 GetSystemMetrics(Int32 Value);
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetCursor(IntPtr cursorHandle);
+        [DllImport("user32.dll")]
+        public static extern IntPtr LoadCursorFromFile(string fileName);
+        [DllImport("user32.dll")]
+        public static extern bool SetSystemCursor(IntPtr hCursor, CursorType cursor);
+
+        public enum CursorType : uint
+        {
+            IDC_ARROW = 32512U,         //Arrow箭头
+            IDC_IBEAM = 32513U,         //Text Select工字钢
+            IDC_WAIT = 32514U,          //Busy等待
+            IDC_CROSS = 32515U,         //交叉
+            IDC_UPARROW = 32516U,       //Alternate Select向上光标
+            IDC_SIZE = 32640U,          //
+            IDC_ICON = 32641U,          //
+            IDC_SIZENWSE = 32642U,      //Diagonal Resize 1反斜线
+            IDC_SIZENESW = 32643U,      //Diagonal Resize 2斜线
+            IDC_SIZEWE = 32644U,        //Horizontal Resize水平调整
+            IDC_SIZENS = 32645U,        //Vertical Resize垂直调整
+            IDC_SIZEALL = 32646U,       //Move十字移动光标
+            IDC_NO = 32648U,            //Unavailable不
+            IDC_HAND = 32649U,          //Link手
+            IDC_APPSTARTING = 32650U,   //Starting程序启动后台运行
+            IDC_HELP = 32651U           //Help帮助
+        }
+        static public IntPtr lcur;
+        static public bool GetCursorFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                lcur=LoadCursorFromFile(fileName);
+                return true;
+            }
+            else return false;
+        }
+        static public void SetMouseCursor(string spath)
+        {
+
+            if (GetCursorFile(spath+"Arrow.cur"))   SetSystemCursor(lcur, CursorType.IDC_ARROW);
+
+            if (GetCursorFile(spath+"Busy.ani"))      SetSystemCursor(lcur, CursorType.IDC_WAIT);
+
+            if (GetCursorFile(spath+"Help.cur"))      SetSystemCursor(lcur, CursorType.IDC_HAND);
+
+            if (GetCursorFile(spath+"Starting.ani"))  SetSystemCursor(lcur, CursorType.IDC_APPSTARTING);
+
+            if (GetCursorFile(spath+"Link.cur"))      SetSystemCursor(lcur, CursorType.IDC_HAND);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("+----------------------------------------+");
-            Console.WriteLine("|            SwapMouse 1.0               |");
-            Console.WriteLine("|----------------------------------------|");
-            Console.WriteLine("| This Program swaps the mouse buttons.  |");
-            Console.WriteLine("|                                        |");
-            Console.WriteLine("| Created by: William Krebbers           |");
-            Console.WriteLine("| Version   : 1.0 2015                   |");
-            Console.WriteLine("+----------------------------------------+");
             try
             {
                 bool swapped = GetSystemMetrics(23) != 0;
@@ -29,12 +72,18 @@ namespace SwapMouse
                 if (swapped)
                 {
                     SwapMouseButton(0);
-                    Console.WriteLine("MouseButtons swapped back to default");
+                    SetMouseCursor(".\\right\\");
+                    Console.WriteLine("MouseButtons swapped to right");
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadLine();
                 }
                 else
                 {
                     SwapMouseButton(1);
-                    Console.WriteLine("MouseButtons swapped");
+                    SetMouseCursor(".\\left\\");
+                    Console.WriteLine("MouseButtons swapped to left");
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadLine();
                 }
             }
             catch (Exception)
